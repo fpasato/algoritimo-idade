662a681c241ae4043344d5d7004e845a57f9191b
Fernando Asato <fp.asato.dev@gmail.com>
2026-02-25 19:33:15 -0300

    20/02

--- criando-pasta-python/desafaio.py
+++ criando-pasta-python/desafaio.py
@@ -1,17 +1,18 @@
 import os
 
 while True:
-
     nome = input('Digite o nome da pasta: ').strip().lower()
-    print()
-    if os.path.exists(nome):
 
-        print(f'A pasta com nome {nome} já existe')
-        print()
+    if nome == '':
+        print("\033[0;30;41mNome invalido, tente novamente\033[m")
+        continue
+
+    if os.path.exists(nome):
+        print(f"\033[0;30;43mA pasta com nome {nome} já existe\033[m")
+
     else:
         os.mkdir(nome)
-        print("Pasta criada com sucesso!!")
-        print()
+        print("\033[0;30;42mPasta criada com sucesso!!\033[m")
 
          
     continuar = input('Deseja Criar uma nova pasta? \n[S]sim \nQualquer outra tecla para não\n ')
